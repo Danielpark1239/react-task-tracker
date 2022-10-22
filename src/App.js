3abafa09401de32897307e39c0a0b055c8479d49
Daniel Park <Danielpark1239@gmail.com>
2022-10-22 18:20:01 -0400

    Added json-server fetching and routing

--- src/App.js
+++ src/App.js
@@ -1,46 +1,126 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Header from "./components/Header"
 import Tasks from "./components/Tasks"
 import AddTask from "./components/AddTask"
+import Footer from "./components/Footer"
+import About from "./components/About"
 
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+
+    getTasks()
+  }, [])
+
+  // Fetch Tasks
+  const fetchTasks = async () => {
+    const res = await fetch("http://localhost:5050/tasks")
+    const data = await res.json()
+    return data
+  }
+
+  // Fetch singular task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5050/tasks/${id}`)
+    const data = await res.json()
+    return data
+  }
+
   // Add Task
-  const addTask = (task) => {
-    // generate a random number: how to stop duplicates?
-    const id = Math.floor(Math.random() * 10000) + 1
-    const newTask = { id, ...task }
+  const addTask = async (task) => {
+    // json-server adds unique id automatically
+    const res = await fetch("http://localhost:5050/tasks", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(task),
+    })
+    const newTask = await res.json()
+
     setTasks([...tasks, newTask])
   }
 
   // Delete Task
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    // delete from server
+    await fetch(`http://localhost:5050/tasks/${id}`, { method: "DELETE" })
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
   // Toggle Reminder
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+
+    const res = await fetch(`http://localhost:5050/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(updatedTask),
+    })
+
+    const data = await res.json()
+
     setTasks(
       tasks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task
+        task.id === id ? { ...task, reminder: data.reminder } : task
       )
     )
   }
 
   return (
-    <div className="container">
-      <Header
-        onAdd={() => setShowAddTask(!showAddTask)}
-        showAdd={showAddTask}
-      />
-      {showAddTask && <AddTask onAdd={addTask} />}
-      {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
-      ) : (
-        "No Tasks To Show"
-      )}
-    </div>
+    <Router>
+      <div className="container">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Header
+                  onAdd={() => setShowAddTask(!showAddTask)}
+                  showAdd={showAddTask}
+                  showButton={true}
+                />
+                {showAddTask && <AddTask onAdd={addTask} />}
+                {tasks.length > 0 ? (
+                  <Tasks
+                    tasks={tasks}
+                    onDelete={deleteTask}
+                    onToggle={toggleReminder}
+                  />
+                ) : (
+                  "No Tasks To Show"
+                )}
+                <Footer showLink={true} />
+              </>
+            }
+          />
+          <Route
+            path="/about"
+            element={
+              <>
+                <Header
+                  onAdd={() => setShowAddTask(!showAddTask)}
+                  showAdd={showAddTask}
+                  showButton={false}
+                />
+                <About />
+                <Footer showLink={false} />
+              </>
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
